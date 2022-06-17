0cfe318b64966454d391c29add51d1d24fc7ed6c
samueltobyknight <56262589+samueltobyknight@users.noreply.github.com>
2022-06-17 15:14:14 +0100

    remove useD3 hook

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import axios from "axios";
 import AsyncSelect from 'react-select/async';
 import GameGrid from './lib/GameGrid';
+import Emoji from './lib/Emoji';
+
+const genStarCount = (team, att) => {
+    switch(att){
+        case "DEF":
+            return Math.min((Math.floor(team.DEF-60)/5), 5)
+        case "MID":
+            return Math.min((Math.floor(team.MID-60)/5), 5)
+        case "ATT":
+            return Math.min((Math.floor(team.ATT-60)/5), 5)
+        default:
+            return 0
+    }
+}
+
 
 function App() {
 
@@ -14,15 +29,17 @@ function App() {
   }
 
   const [homeTeam, setHomeTeam] = React.useState('')
+
   const handleHomeChange = (selectedOption) => {
     setHomeTeam(selectedOption);
-    predictResults()
+    predictResults();
   };
 
   const [awayTeam, setAwayTeam] = React.useState('')
+
   const handleAwayChange = (selectedOption) => {
     setAwayTeam(selectedOption);
-    predictResults()
+    predictResults();
   };
 
   ////////////////////////////
@@ -41,257 +58,279 @@ function App() {
 
   const predictResults = async () => {
 
-    let res = await axios.post('/predict', {
-      homeTeam: homeTeam,
-      awayTeam: awayTeam
-    })
+    if(homeTeam && awayTeam){
     
-    setResults(res.data)
-    console.log(res.data)
+        let res = await axios.post('/predict', {
+          homeTeam: homeTeam,
+          awayTeam: awayTeam
+        })
+
+        setResults(res.data)
+
+    }
   }
+
+  React.useEffect(() => {
+    predictResults()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [homeTeam, awayTeam]);
 
   ////////////////////////////
   // UI
   ////////////////////////////
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <AsyncSelect
-        onChange={handleHomeChange}
-        value={homeTeam}
-        loadOptions={loadTeams}
-        defaultOptions
-        getOptionLabel={(t) => t.Name}
-        getOptionValue={(t) => t}
-      />
-      <AsyncSelect
-        onChange={handleAwayChange}
-        value={awayTeam}
-        loadOptions={loadTeams}
-        defaultOptions
-        getOptionLabel={(t) => t.Name}
-        getOptionValue={(t) => t}
-      />
-      <GameGrid 
-        data={ {scores: [
-          {
-              "home": "0",
-              "away": "0",
-              "result": "DRAW",
-              "probability": 0.06659846573877168
-          },
-          {
-              "home": "0",
-              "away": "1",
-              "result": "AWAY",
-              "probability": 0.12626091758350685
-          },
-          {
-              "home": "0",
-              "away": "2",
-              "result": "AWAY",
-              "probability": 0.11968608534887201
-          },
-          {
-              "home": "0",
-              "away": "3",
-              "result": "AWAY",
-              "probability": 0.07563575121685784
-          },
-          {
-              "home": "0",
-              "away": "4",
-              "result": "AWAY",
-              "probability": 0.03584857115258843
-          },
-          {
-              "home": "1",
-              "away": "0",
-              "result": "HOME",
-              "probability": 0.054159236984216055
-          },
-          {
-              "home": "1",
-              "away": "1",
-              "result": "DRAW",
-              "probability": 0.10267796534641073
-          },
-          {
-              "home": "1",
-              "away": "2",
-              "result": "AWAY",
-              "probability": 0.09733117705065952
-          },
-          {
-              "home": "1",
-              "away": "3",
-              "result": "AWAY",
-              "probability": 0.06150854271479444
-          },
-          {
-              "home": "1",
-              "away": "4",
-              "result": "AWAY",
-              "probability": 0.029152792621591365
-          },
-          {
-              "home": "2",
-              "away": "0",
-              "result": "HOME",
-              "probability": 0.022021700636602203
-          },
-          {
-              "home": "2",
-              "away": "1",
-              "result": "HOME",
-              "probability": 0.04174991267866366
-          },
-          {
-              "home": "2",
-              "away": "2",
-              "result": "DRAW",
-              "probability": 0.03957585377804369
-          },
-          {
-              "home": "2",
-              "away": "3",
-              "result": "AWAY",
-              "probability": 0.025010003642658755
-          },
-          {
-              "home": "2",
-              "away": "4",
-              "result": "AWAY",
-              "probability": 0.011853824159685458
-          },
-          {
-              "home": "3",
-              "away": "0",
-              "result": "HOME",
-              "probability": 0.005969499398837039
-          },
-          {
-              "home": "3",
-              "away": "1",
-              "result": "HOME",
-              "probability": 0.011317294824294529
-          },
-          {
-              "home": "3",
-              "away": "2",
-              "result": "HOME",
-              "probability": 0.010727965075677547
-          },
-          {
-              "home": "3",
-              "away": "3",
-              "result": "DRAW",
-              "probability": 0.006779549144429708
-          },
-          {
-              "home": "3",
-              "away": "4",
-              "result": "AWAY",
-              "probability": 0.003213257566382045
-          },
-          {
-              "home": "4",
-              "away": "0",
-              "result": "HOME",
-              "probability": 0.0012136298075052078
-          },
-          {
-              "home": "4",
-              "away": "1",
-              "result": "HOME",
-              "probability": 0.0023008640124436677
-          },
-          {
-              "home": "4",
-              "away": "2",
-              "result": "HOME",
-              "probability": 0.0021810502556133277
-          },
-          {
-              "home": "4",
-              "away": "3",
-              "result": "HOME",
-              "probability": 0.0013783170703944205
-          },
-          {
-              "home": "4",
-              "away": "4",
-              "result": "DRAW",
-              "probability": 0.0006532717236746222
-          },
-          {
-              "home": "0",
-              "away": "5+",
-              "result": "AWAY",
-              "probability": 0.019397918403765722
-          },
-          {
-              "home": "1",
-              "away": "5+",
-              "result": "AWAY",
-              "probability": 0.015774784721781045
-          },
-          {
-              "home": "2",
-              "away": "5+",
-              "result": "AWAY",
-              "probability": 0.00641418908562448
-          },
-          {
-              "home": "3",
-              "away": "5+",
-              "result": "AWAY",
-              "probability": 0.0017387166650981343
-          },
-          {
-              "home": "4",
-              "away": "5+",
-              "result": "AWAY",
-              "probability": 0.0003534900048704654
-          },
-          {
-              "home": "5+",
-              "away": "0",
-              "result": "HOME",
-              "probability": 0.00022759836256332571
-          },
-          {
-              "home": "5+",
-              "away": "1",
-              "result": "HOME",
-              "probability": 0.00043149309490803285
-          },
-          {
-              "home": "5+",
-              "away": "2",
-              "result": "HOME",
-              "probability": 0.0004090237927382039
-          },
-          {
-              "home": "5+",
-              "away": "3",
-              "result": "HOME",
-              "probability": 0.00025848302865905354
-          },
-          {
-              "home": "5+",
-              "away": "4",
-              "result": "HOME",
-              "probability": 0.00012251147235985087
-          },
-          {
-              "home": "5+",
-              "away": "5+",
-              "result": "DRAW",
-              "probability": 0.00006629183445683846
-          }]
+        <header className="App-header"></header>
+
+        <AsyncSelect
+            onChange={handleHomeChange}
+            loadOptions={loadTeams}
+            defaultOptions
+            getOptionLabel={(t) => t.Name}
+            getOptionValue={(t) => t}
+        />
+        <div>
+            <div>Def</div> {homeTeam ? <Emoji symbol="⭐" label="starHomeDef" N={genStarCount(homeTeam, "DEF")}/> : "-"}
+            <div>Mid</div> {homeTeam ? <Emoji symbol="⭐" label="starHomeMid" N={genStarCount(homeTeam, "MID")}/> : "-"}
+            <div>Att</div> {homeTeam ? <Emoji symbol="⭐" label="starHomeAtt" N={genStarCount(homeTeam, "ATT")}/> : "-"}
+        </div>
+
+        <AsyncSelect
+            onChange={handleAwayChange}
+            loadOptions={loadTeams}
+            defaultOptions
+            getOptionLabel={(t) => t.Name}
+            getOptionValue={(t) => t}
+        />
+        <div>
+            <div>Def</div> {awayTeam ? <Emoji symbol="⭐" label="starAwayDef" N={genStarCount(awayTeam, "DEF")}/> : "-"}
+            <div>Mid</div> {awayTeam ? <Emoji symbol="⭐" label="starAwayMid" N={genStarCount(awayTeam, "MID")}/> : "-"}
+            <div>Att</div> {awayTeam ? <Emoji symbol="⭐" label="starAwayAtt" N={genStarCount(awayTeam, "ATT")}/> : "-"}
+        </div>
+
+        <GameGrid 
+            data={ results && results.scores != null ? results : 
+                {scores: [
+                    {
+                        "home": "0",
+                        "away": "0",
+                        "result": "DRAW",
+                        "probability": 0
+                    },
+                    {
+                        "home": "0",
+                        "away": "1",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "0",
+                        "away": "2",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "0",
+                        "away": "3",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "0",
+                        "away": "4",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "0",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "1",
+                        "result": "DRAW",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "2",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "3",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "4",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "0",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "1",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "2",
+                        "result": "DRAW",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "3",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "4",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "0",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "1",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "2",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "3",
+                        "result": "DRAW",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "4",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "0",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "1",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "2",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "3",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "4",
+                        "result": "DRAW",
+                        "probability": 0
+                    },
+                    {
+                        "home": "0",
+                        "away": "5+",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "1",
+                        "away": "5+",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "2",
+                        "away": "5+",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "3",
+                        "away": "5+",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "4",
+                        "away": "5+",
+                        "result": "AWAY",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "0",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "1",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "2",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "3",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "4",
+                        "result": "HOME",
+                        "probability": 0
+                    },
+                    {
+                        "home": "5+",
+                        "away": "5+",
+                        "result": "DRAW",
+                        "probability": 0
+                    }
+                ]
+            }
         }
-      } />
+        />
     </div>
   );
 }
--- client/src/lib/GameGrid.js
+++ client/src/lib/GameGrid.js
@@ -1,17 +1,168 @@
 import React from 'react';
 import * as d3 from 'd3';
-import { useD3 } from './useD3';
+
+const boxes = [
+  {
+      "home": "0",
+      "away": "0"
+  },
+  {
+      "home": "0",
+      "away": "1"
+  },
+  {
+      "home": "0",
+      "away": "2"
+  },
+  {
+      "home": "0",
+      "away": "3"
+  },
+  {
+      "home": "0",
+      "away": "4"
+  },
+  {
+      "home": "1",
+      "away": "0"
+  },
+  {
+      "home": "1",
+      "away": "1"
+  },
+  {
+      "home": "1",
+      "away": "2"
+  },
+  {
+      "home": "1",
+      "away": "3"
+  },
+  {
+      "home": "1",
+      "away": "4"
+  },
+  {
+      "home": "2",
+      "away": "0"
+  },
+  {
+      "home": "2",
+      "away": "1"
+  },
+  {
+      "home": "2",
+      "away": "2"
+  },
+  {
+      "home": "2",
+      "away": "3"
+  },
+  {
+      "home": "2",
+      "away": "4"
+  },
+  {
+      "home": "3",
+      "away": "0"
+  },
+  {
+      "home": "3",
+      "away": "1"
+  },
+  {
+      "home": "3",
+      "away": "2"
+  },
+  {
+      "home": "3",
+      "away": "3"
+  },
+  {
+      "home": "3",
+      "away": "4"
+  },
+  {
+      "home": "4",
+      "away": "0"
+  },
+  {
+      "home": "4",
+      "away": "1"
+  },
+  {
+      "home": "4",
+      "away": "2"
+  },
+  {
+      "home": "4",
+      "away": "3"
+  },
+  {
+      "home": "4",
+      "away": "4"
+  },
+  {
+      "home": "0",
+      "away": "5+"
+  },
+  {
+      "home": "1",
+      "away": "5+"
+  },
+  {
+      "home": "2",
+      "away": "5+"
+  },
+  {
+      "home": "3",
+      "away": "5+"
+  },
+  {
+      "home": "4",
+      "away": "5+"
+  },
+  {
+      "home": "5+",
+      "away": "0"
+  },
+  {
+      "home": "5+",
+      "away": "1"
+  },
+  {
+      "home": "5+",
+      "away": "2"
+  },
+  {
+      "home": "5+",
+      "away": "3"
+  },
+  {
+      "home": "5+",
+      "away": "4"
+  },
+  {
+      "home": "5+",
+      "away": "5+"
+  }]
+
+
 
 const GameGrid = ({ data }) => {
-  const ref = useD3( (svg) => {
-      
-    const margin = {top: 0, right: 0, bottom: 0, left: 0};
-    const width = 420 - margin.left - margin.right;
-    const height = 380 - margin.top - margin.bottom;
+  const svgRef = React.useRef(null);
 
-    svg
-      .attr("viewBox", `-40 -40 ${width + 50} ${height + 50}`)
+  const margin = {top: 25, right: 25, bottom: 25, left: 25};
+  const width = 420 - margin.left - margin.right;
+  const height = 380 - margin.top - margin.bottom;
 
+  React.useEffect(() => {
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove();
+  
+    const svg = svgEl
+      .attr("viewBox", `-30 -40 ${width + 50} ${height + 50}`)
+  
     var x = d3
       .scaleBand()
       .range([ 0, width ])
@@ -30,7 +181,7 @@ const GameGrid = ({ data }) => {
       .range([ height, 0 ])
       .domain((['0', '1', '2', '3', '4', '5+']).reverse())
       .padding(0.05)
-
+  
     svg.append('g')
       .style('font-size', 15)
       .style('color', "grey")
@@ -38,70 +189,62 @@ const GameGrid = ({ data }) => {
       .call(d3.axisLeft(y).tickSize(0))
       .select('.domain')
       .remove()
-
+  
     // Build color scale
     var homeColour = d3.scaleLinear()
       .domain([0,0.12])
-      .range(['#cde4f1', '#077bbd'])
+      .range(['#FFFFFF', '#077bbd'])
     var awayColour = d3.scaleLinear()
       .domain([0,0.12])
-      .range(['#ffddf4', '#ff59cc'])
+      .range(['#FFFFFF', '#ff59cc'])
     var drawColour = d3.scaleLinear()
       .domain([0,0.12])
-      .range(['#e4e4e4', '#7C7C7C'])
+      .range(['#FFFFFF', '#7C7C7C'])
     
     svg.selectAll()
-      .data(data.scores, function(d) {return d.away+':'+d.home;})
-      .enter()
-      .append('rect')
-      .attr('x', function(d) { return x(d.away) })
-      .attr('y', function(d) { return y(d.home) })
-      .attr('rx', 4)
-      .attr('ry', 4)
-      .attr('width', x.bandwidth() )
-      .attr('height', y.bandwidth() )
-      .style('fill', function(d) { 
-          if(d.result === 'HOME'){
-              return homeColour(d.probability)
-          } else if(d.result === 'AWAY'){
-              return awayColour(d.probability)
-          } else{
-              return drawColour(d.probability)
-          }
-      })
-      .attr("text-anchor", "middle")
-      .attr("dominant-baseline", "central") 
-      .style('stroke-width', 1)
-      .style('stroke', 'grey')
-      .style('opacity', 0.8)
+      .data(boxes, function(d) {return d.away+':'+d.home;})
+      .join('rect')
+        .attr('class', 'gridBox')
+        .attr('x', function(d) { return x(d.away) })
+        .attr('y', function(d) { return y(d.home) })
+        .attr('rx', 4)
+        .attr('ry', 4)
+        .attr('width', x.bandwidth() )
+        .attr('height', y.bandwidth() )
+        .style('stroke-width', 1)
+        .style('stroke', 'grey')
+        .style('opacity', 0.8)
+        .style('fill', 'transparent')
 
-      svg.selectAll()
+    svg.selectAll('.gridBox')
         .data(data.scores)
-        .enter()
-        .append("text")
-        .attr("class", "probLabel")
-        .attr('opacity', .5)
-        .attr("x", function(d) { return x(d.away) + width/20 })
-        .attr("y", function(d) { return y(d.home) + height/10})
+        .join("rect")
+          .style('fill', function(d) { 
+            if(d.result === 'HOME'){
+                return homeColour(d.probability)
+            } else if(d.result === 'AWAY'){
+                return awayColour(d.probability)
+            } else{
+                return drawColour(d.probability)
+            }
+        })
+
+    svg.selectAll()
+      .data(data.scores)
+      .enter()
+      .append("text")
+        .attr('fill', 'grey')
+        .attr('font-weight', function(d) { return d.probability*800 + 500 }) 
+        .attr('opacity', 1)
+        .attr("x", function(d) { return x(d.away) + 18 })
+        .attr("y", function(d) { return y(d.home) + 30 })
         .text(function(d) {
             return `${Math.round(d.probability*100)}%`;
         })
-    
-    }, 
-    [data.scores]
-  );
 
-  return (
-    <svg
-      ref={ref}
-      style={{
-        height: "40%",
-        width: "40%"
-      }}
-    >
-      <g className="plot-area"/>
-    </svg>
-  );
-}
+  }, [data, height, width])
+
+  return <svg ref={svgRef}/>;
+};
   
 export default GameGrid;
